2c42a8e31272acbcd38fa9724d7f2a060b195ef0
steve-warlock <89355017+steve-warlock@users.noreply.github.com>
2023-05-06 13:23:04 +0300

    Update DLL.cpp

--- DLL.cpp
+++ DLL.cpp
@@ -18,80 +18,78 @@ DLL::DLL()
 DLL::DLL(const DLL& p)
 {
     head = tail = nullptr;
-       DLL::node* temp = p.head;
-       while (temp != nullptr) {
-           AddToTail(*this, temp -> info);
-           temp = temp -> next;
-       }
+    DLL::node* temp = p.head;
+    while (temp != nullptr) {
+        AddToTail(*this, temp -> info);
+        temp = temp -> next;
+    }
 }
 DLL::~DLL()
 {
     DLL::node* q = head;
-       while (q != nullptr) {
-           head = q -> next;
-           delete q;
-           q = head;
-       }
-       tail = nullptr;
+    while (q != nullptr) {
+        head = q -> next;
+        delete q;
+        q = head;
+    }
+    tail = nullptr;
 }
 
 void DLL::AddToHead(DLL& p, int x)
 {
     if (p.head == nullptr) {
-           p.head = new DLL::node;
-           p.head -> info = x;
-           p.head -> prev = nullptr;
-           p.head -> next = nullptr;
-           p.tail = p.head;
-       } else {
-           DLL::node* q = new DLL::node;
-           q -> info = x;
-           q -> prev = nullptr;
-           q -> next = p.head;
-           p.head -> prev = q;
-           p.head = q;
-       }
+        p.head = new DLL::node;
+        p.head -> info = x;
+        p.head -> prev = nullptr;
+        p.head -> next = nullptr;
+        p.tail = p.head;
+    } else {
+        DLL::node* q = new DLL::node;
+        q -> info = x;
+        q -> prev = nullptr;
+        q -> next = p.head;
+        p.head -> prev = q;
+        p.head = q;
+    }
 }
 
 void DLL::AddToTail(DLL& p, int x)
 {
     DLL::node* q = new DLL::node;
-        q -> info = x;
-        q -> next = nullptr;
-        q -> prev = nullptr;
-        if (p.head == nullptr) {
-            p.head = q;
-            p.tail = q;
-        } else {
-            q -> prev = p.tail;
-            p.tail -> next = q;
-            p.tail = q;
-        }
+    q -> info = x;
+    q -> next = nullptr;
+    q -> prev = nullptr;
+    if (p.head == nullptr) {
+        p.head = q;
+        p.tail = q;
+    } else {
+        q -> prev = p.tail;
+        p.tail -> next = q;
+        p.tail = q;
+    }
 }
 
 void DLL::quickSort(DLL::node* head, DLL::node* tail, bool isAscending) {
-    if (head == nullptr || tail == nullptr || head == tail || head == tail->next)
+    if (head == nullptr || tail == nullptr || head == tail || head == tail -> next)
         return;
-
-    DLL::node* pivot = tail;
-    DLL::node* i = head->prev;
     
-    for (DLL::node* j = head; j != tail; j = j->next) {
-        if ((isAscending && j->info <= pivot->info) || (!isAscending && j->info >= pivot->info)) {
-            i = (i == nullptr) ? head : i->next;
-            std::swap(i->info, j->info);
+    DLL::node* pivot = tail;
+    DLL::node* i = head -> prev;
+    
+    for (DLL::node* j = head; j != tail; j = j -> next) {
+        if ((isAscending && j -> info <= pivot -> info) || (!isAscending && j -> info >= pivot -> info)) {
+            i = (i == nullptr) ? head : i -> next;
+            std::swap(i -> info, j -> info);
         }
     }
     
-    i = (i == nullptr) ? head : i->next;
-    std::swap(i->info, tail->info);
-
+    i = (i == nullptr) ? head : i -> next;
+    std::swap(i -> info, tail -> info);
+    
     DLL::node* pivotIndex = i;
-    quickSort(head, pivotIndex->prev, isAscending);
-    quickSort(pivotIndex->next, tail, isAscending);
+    quickSort(head, pivotIndex -> prev, isAscending);
+    quickSort(pivotIndex -> next, tail, isAscending);
 }
-
-
 
 
 void DLL::sortAscending(DLL& p)
@@ -105,41 +103,40 @@ void DLL::sortDescending(DLL& p)
 }
 
 
-
 void DLL::DeleteNode(DLL& p, DLL::node* temp)
 {
     if (p.head == nullptr || temp == nullptr) {
-           return;
-       }
-       if (p.head == temp) {
-           p.head = temp -> next;
-       }
-       if (temp -> prev != nullptr) {
-           temp -> prev -> next = temp -> next;
-       }
-       if (temp -> next != nullptr) {
-           temp -> next -> prev = temp -> prev;
-       }
-       delete temp;
-   
+        return;
+    }
+    if (p.head == temp) {
+        p.head = temp -> next;
+    }
+    if (temp -> prev != nullptr) {
+        temp -> prev -> next = temp -> next;
+    }
+    if (temp -> next != nullptr) {
+        temp -> next -> prev = temp -> prev;
+    }
+    delete temp;
+    
 }
 void DLL::DeleteNodes(DLL& p, int x)
 {
-    if (p.head == nullptr) {
-            return;
+    if (p.head == nullptr)
+        return;
+    
+    DLL::node* temp = p.head;
+    while (temp != nullptr) {
+        if (temp -> info == x) {
+            DLL::node* urm = temp -> next;
+            DeleteNode(p, temp);
+            temp = urm;
+        } else {
+            temp = temp -> next;
         }
-        DLL::node* temp = p.head;
-        while (temp != nullptr) {
-            if (temp -> info == x) {
-                DLL::node* urm = temp -> next;
-                DeleteNode(p, temp);
-                temp = urm;
-            } else {
-                temp = temp -> next;
-            }
-        }
+    }
 }
-int DLL::search(DLL & p , int x)
+int DLL::search(DLL& p , int x)
 {
     int poz = 0;
     DLL::node *q = p.head;
@@ -153,15 +150,15 @@ int DLL::search(DLL & p , int x)
     return -1;
 }
 
-void DLL::reset(DLL & p)
+void DLL::reset(DLL& p)
 {
     DLL::node* q = p.head;
-        while (q != nullptr) {
-            DLL::node* temp = q;
-            q = q -> next;
-            delete temp;
-        }
-        p.head = p.tail = nullptr;
+    while (q != nullptr) {
+        DLL::node* temp = q;
+        q = q -> next;
+        delete temp;
+    }
+    p.head = p.tail = nullptr;
 }
 
 std::ostream& operator<< (std::ostream& out, DLL& p)
@@ -173,15 +170,15 @@ std::ostream& operator<< (std::ostream& out, DLL& p)
 }
 
 
-DLL& DLL::operator += (const DLL& p)
+DLL& DLL::operator+= (const DLL& p)
 {
     DLL::node* temp = p.head;
-       while(temp != nullptr)
-       {
-           AddToTail(*this, temp->info);
-           temp = temp->next;
-       }
-       return *this;
+    while(temp != nullptr)
+    {
+        AddToTail(*this, temp -> info);
+        temp = temp -> next;
+    }
+    return *this;
 }
 
 }
